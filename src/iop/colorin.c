eb2dc3fd2b42905bb2cc9e89859b2907eedd4768
Nicolas Boichat <nicolas@boichat.ch>
2013-11-28 12:27:27 +0800

    colorin.
    Fix colorin.c

--- src/iop/colorin.c
+++ src/iop/colorin.c
@@ -32,8 +32,8 @@
 #include "common/imageio_j2k.h"
 #endif
 #include "common/imageio_jpeg.h"
+#include "common/vector.h"
 #include "external/adobe_coeff.c"
-#include <xmmintrin.h>
 #include <stdlib.h>
 #include <math.h>
 #include <assert.h>
@@ -199,34 +199,36 @@ error:
 }
 #endif
 
-static inline __m128
-lab_f_m(const __m128 x)
+static inline v4sf
+lab_f_m(const v4sf x)
 {
-  const __m128 epsilon = _mm_set1_ps(216.0f/24389.0f);
-  const __m128 kappa   = _mm_set1_ps(24389.0f/27.0f);
+  const v4sf epsilon = v4sf_setall(216.0f/24389.0f);
+  const v4sf kappa   = v4sf_setall(24389.0f/27.0f);
 
   // calculate as if x > epsilon : result = cbrtf(x)
   // approximate cbrtf(x):
-  const __m128 a = _mm_castsi128_ps(_mm_add_epi32(_mm_cvtps_epi32(_mm_div_ps(_mm_cvtepi32_ps(_mm_castps_si128(x)),_mm_set1_ps(3.0f))),_mm_set1_epi32(709921077)));
-  const __m128 a3 = _mm_mul_ps(_mm_mul_ps(a,a),a);
-  const __m128 res_big = _mm_div_ps(_mm_mul_ps(a,_mm_add_ps(a3,_mm_add_ps(x,x))),_mm_add_ps(_mm_add_ps(a3,a3),x));
+  /* FIXME: Reoptimize based on SSE code */
+  const v4sf a = v4sf_set(cbrtf(x[3]),cbrtf(x[2]),cbrtf(x[1]),cbrtf(x[0]));
+
+  const v4sf a3 = a*a*a;
+  const v4sf res_big = (a*(a3+x+x))/(a3+a3+x);
 
   // calculate as if x <= epsilon : result = (kappa*x+16)/116
-  const __m128 res_small = _mm_div_ps(_mm_add_ps(_mm_mul_ps(kappa,x),_mm_set1_ps(16.0f)),_mm_set1_ps(116.0f));
+  const v4sf res_small = ((kappa*x)+v4sf_setall(16.0f))/v4sf_setall(116.0f);
 
   // blend results according to whether each component is > epsilon or not
-  const __m128 mask = _mm_cmpgt_ps(x,epsilon);
-  return _mm_or_ps(_mm_and_ps(mask,res_big),_mm_andnot_ps(mask,res_small));
+  const v4si mask = (x > epsilon);
+  return (v4sf)((mask & (v4si)res_big) | (~mask & (v4si)res_small));
 }
 
-static inline __m128
-dt_XYZ_to_Lab_SSE(const __m128 XYZ)
+static inline v4sf
+dt_XYZ_to_Lab_SSE(const v4sf XYZ)
 {
-  const __m128 d50_inv  = _mm_set_ps(0.0f, 1.0f/0.8249f, 1.0f, 1.0f/0.9642f);
-  const __m128 coef = _mm_set_ps(0.0f,200.0f,500.0f,116.0f);
-  const __m128 f = lab_f_m(_mm_mul_ps(XYZ,d50_inv));
+  const v4sf d50_inv  = v4sf_set(0.0f, 1.0f/0.8249f, 1.0f, 1.0f/0.9642f);
+  const v4sf coef = v4sf_set(0.0f,200.0f,500.0f,116.0f);
+  const v4sf f = lab_f_m(XYZ*d50_inv);
   // because d50_inv.z is 0.0f, lab_f(0) == 16/116, so Lab[0] = 116*f[0] - 16 equal to 116*(f[0]-f[3])
-  return _mm_mul_ps(coef,_mm_sub_ps(_mm_shuffle_ps(f,f,_MM_SHUFFLE(3,1,0,1)),_mm_shuffle_ps(f,f,_MM_SHUFFLE(3,2,1,3))));
+  return coef*(__builtin_shuffle(f, v4si_set(3,1,0,1))-__builtin_shuffle(f, v4si_set(3,2,1,3)));
 }
 
 void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void *i, void *o, const dt_iop_roi_t *roi_in, const dt_iop_roi_t *roi_out)
@@ -250,9 +252,9 @@ void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void 
       float *buf_in  = in + ch*roi_in->width *j;
       float *buf_out = out + ch*roi_out->width*j;
       float cam[3];
-      const __m128 m0 = _mm_set_ps(0.0f,mat[6],mat[3],mat[0]);
-      const __m128 m1 = _mm_set_ps(0.0f,mat[7],mat[4],mat[1]);
-      const __m128 m2 = _mm_set_ps(0.0f,mat[8],mat[5],mat[2]);
+      const v4sf m0 = v4sf_set(0.0f,mat[6],mat[3],mat[0]);
+      const v4sf m1 = v4sf_set(0.0f,mat[7],mat[4],mat[1]);
+      const v4sf m2 = v4sf_set(0.0f,mat[8],mat[5],mat[2]);
 
       for(int i=0; i<roi_out->width; i++, buf_in+=ch, buf_out+=ch )
       {
@@ -287,14 +289,14 @@ void process (struct dt_iop_module_t *self, dt_dev_pixelpipe_iop_t *piece, void 
 
 #if 0
         __attribute__((aligned(16))) float XYZ[4];
-        _mm_store_ps(XYZ,_mm_add_ps(_mm_add_ps( _mm_mul_ps(m0,_mm_set1_ps(cam[0])), _mm_mul_ps(m1,_mm_set1_ps(cam[1]))), _mm_mul_ps(m2,_mm_set1_ps(cam[2]))));
+        *XYZ = m0*cam[0]+m1*cam[1]+m2*cam[2];
         dt_XYZ_to_Lab(XYZ, buf_out);
 #endif
-        __m128 xyz = _mm_add_ps(_mm_add_ps( _mm_mul_ps(m0,_mm_set1_ps(cam[0])), _mm_mul_ps(m1,_mm_set1_ps(cam[1]))), _mm_mul_ps(m2,_mm_set1_ps(cam[2])));
-        _mm_stream_ps(buf_out,dt_XYZ_to_Lab_SSE(xyz));
+        v4sf xyz = m0*cam[0]+m1*cam[1]+m2*cam[2];
+        *(v4sf*)buf_out = dt_XYZ_to_Lab_SSE(xyz);
       }
     }
-    _mm_sfence();
+    vector_sfence();
   }
   else
   {
